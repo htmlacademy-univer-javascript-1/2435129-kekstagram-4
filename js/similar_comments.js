4f8e07ab0e3df0490fc7885e6da31bd6a77e5846
алена <sasmurka@mail.ru>
2023-11-30 21:39:45 +0500

    1

--- js/similar_comments.js
+++ js/similar_comments.js
@@ -1,20 +1,19 @@
 import {createComment} from './create_comment.js';
 
-const comment = createComment();
-const similarComment = document.createDocumentFragment('.social__comments');
+const comment = Array.from({ length: 25 }, createComment);
 const liComment = document.querySelector('.social__comment');
 const commentsListFragment = document.createDocumentFragment('.social__comments');
 
-const createCommentPicture = function () {
+const createCommentPicture = function (ulEl) {
   comment.forEach(({avatar, name, message}) => {
     const commentElement = liComment.cloneNode(true);
     commentElement.querySelector('.social__picture').src = avatar;
     commentElement.querySelector('.social__picture').alt = name;
     commentElement.querySelector('.social__text').textContent = message;
 
-    commentsListFragment.appendChild(commentsListFragment);
+    commentsListFragment.appendChild(commentElement);
   });
-  similarComment.appendChild(commentsListFragment);
+  ulEl.appendChild(commentsListFragment);
 };
 
 export {createCommentPicture};
